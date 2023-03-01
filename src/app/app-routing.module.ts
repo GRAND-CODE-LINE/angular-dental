26a6a460ded3ab4dba007ba644924d166b1c4a46
ConsultorCarlos <52987839+cmb1197@users.noreply.github.com>
2023-02-28 21:51:39 -0500

    xd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'adm', loadChildren: () => import('./view/adm/adm-routing.module').then(m => m.AdmRoutingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
