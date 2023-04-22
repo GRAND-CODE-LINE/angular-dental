f9abc5c525aaba7f56a9357e55fc1e011a0789fd
ConsultorCarlos <52987839+cmb1197@users.noreply.github.com>
2023-04-21 21:38:06 -0500

    add shared table component

---
+++ src/app/models/utils/filter_i.ts
@@ -0,0 +1,24 @@
+// DATOS NECESARIOS PARA QUE SE HAGA LA PAGINACION
+export interface Filter_I {
+    page: number,
+    size: number,
+    sortFiled: string,
+    sortOrder: number
+}
+
+// OBJETO DE PAGINACION QUE DEVUELVE SPRING DATA
+export interface Paginate_I {
+    content: any[],
+    last: boolean,
+    totalPages: number,
+    totalElements: number,
+    size: number,
+    number: 0,
+    sort: {
+        empty: boolean,
+        sorted: boolean,
+        unsorted: boolean
+    },
+    first: boolean,
+    empty: false
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    URL_SECURITY: 'http://localhost:8081',
+    URL_ADM: 'http://localhost:8080'
+};
