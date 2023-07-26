6f26a03a1d40ce459360c333280708ed0315428e
AlfredoRomel <97257241+AlfredoRomel@users.noreply.github.com>
2023-07-25 22:35:59 -0500

    Menu completo
    falta corregir  las direcciones para las paginas

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { itemsMenu } from '../models/menu';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private http: HttpClient){}
+  recibirMenu(){
+    return this.http.get<itemsMenu[]>('assets/menu/menu.json');
+  }
 }
