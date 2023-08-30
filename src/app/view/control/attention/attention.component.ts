2a5e79e5c02c841d63016b86751b0dea09cb2d16
AlfredoRomel <97257241+AlfredoRomel@users.noreply.github.com>
2023-08-30 00:02:52 -0500

    modal

--- src/app/view/control/attention/attention.component.ts
+++ src/app/view/control/attention/attention.component.ts
@@ -1,8 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 import { Form, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Consultation } from 'src/app/models/consultation';
+import { Patient } from 'src/app/models/patient';
 import { Procedure } from 'src/app/models/procedure';
 import { ConsultationService } from 'src/app/services/consultation/consultation.service';
 import { PatientService } from 'src/app/services/patient/patient.service';
@@ -14,28 +16,35 @@ import { PatientService } from 'src/app/services/patient/patient.service';
 })
 export class AttentionComponent {
   modalRef?: BsModalRef;
+  
   @ViewChild('modalActions') modalAction!: TemplateRef<any>;
   config = {
     animated: true
   };
   Cards:any[]=[];
   items:Procedure[]=[];
+  consultation:Consultation|undefined;
+  patient :Patient|undefined;
+  procedures:Procedure[]=[];
   constructor(
     private route : ActivatedRoute,
     private modalService: BsModalService,
     private consultationService: ConsultationService,
-    private patientService: PatientService
+    private patientService: PatientService,
+    private location :Location
     ) {
 
   }
 
   ngOnInit(): void {
     console.log('Init');
-    this.initData();
+    let state = this.location.getState();
+    this.consultation=state as unknown as Consultation;
+    this.patient=this.consultation.patient;
   }
 
   initData() { 
-    //llenarData(this.route.snapshot.paramMap.get())
+    
   }
 
   ngOnChanges() {
@@ -49,6 +58,11 @@ export class AttentionComponent {
   onProcedureClick(){
  this.modalRef = this.modalService.show(this.modalAction,this.config)
   }
+  onAddProcedure(event : Procedure){
+    this.modalAction
+  this.procedures.push()
+  }
+
   onEditClick(event: Procedure): void {
 
   }
