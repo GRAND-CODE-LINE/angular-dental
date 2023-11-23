dfd5f36d29f834643e626c69a318653dc8e1d849
ConsultorCarlos <52987839+cmb1197@users.noreply.github.com>
2023-11-22 21:25:14 -0500

    changes modal

--- src/app/security/services/auth-interceptor.service.ts
+++ src/app/security/services/auth-interceptor.service.ts
@@ -15,29 +15,17 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-
-    console.log('ahce login');
-
     const token: any = localStorage.getItem('minita_user');
     const loginobj: LoginResponse = JSON.parse(token)
 
     let request = req;
-    console.log("--------------Aqui token", token);
-
     if (token) {
-
       request = req.clone({
-        //headers: new HttpHeaders({ Authorization: loginobj.tokenType + ' ' + loginobj.accessToken }),
-        // setHeaders: {
-        //   'Authorization': loginobj.tokenType + ' ' + loginobj.accessToken
-        // }
         setHeaders: {
           Authorization: `Bearer ${loginobj.accessToken}`,
           'Access-Control-Allow-Origin': '*'
         }
       });
-
-
     }
     console.log(loginobj);
     console.log(request.headers);
@@ -45,13 +33,17 @@ export class AuthInterceptorService implements HttpInterceptor {
       .pipe(
         tap(event => {
           // Realizar acciones en caso de éxito (por ejemplo, registro de la respuesta).
-          if (event instanceof HttpResponse) {
-
+          if (event instanceof HttpResponse && event.body.error) {
+            let message: Message_I = {
+              title: 'Error',
+              message: event.body.error,
+              type: 'danger'
+            }
+            this.messageService.openModal(message);
+            throw new Error('Error Login:' + event.body.error)
           }
         }),
-        catchError((err: HttpErrorResponse) => {
-          console.log(err);
-
+        catchError((err: HttpErrorResponse, res: any) => {
           if ((err.status === 401 || err.status === 403) && this.router.url == '/security/login') {
             let message: Message_I = {
               title: 'Error',
@@ -68,6 +60,13 @@ export class AuthInterceptorService implements HttpInterceptor {
             this.messageService.openModal(message);
             this.loginService.logOut();
             this.router.navigateByUrl('/security/login');
+          } else if (err.status === 500) {
+            let message: Message_I = {
+              title: 'Error',
+              message: 'Error no controlado.',
+              type: 'danger'
+            }
+            this.messageService.openModal(message);
           }
           return throwError(() => new Error('Error Login:' + err.message));
 
